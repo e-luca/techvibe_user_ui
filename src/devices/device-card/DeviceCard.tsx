f36a1d23d238ee7b7814aae75ffb833c81964e77
e-luca <eldar.lucic@gmail.com>
2023-12-17 19:31:58 +0100

    Add device preview modal

--- src/devices/device-card/DeviceCard.tsx
+++ src/devices/device-card/DeviceCard.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DeviceCard.css'
 import { Device } from '../../data-models/model/Device.model'
+import DevicePreviewModal from '../device-preview-modal/DevicePreviewModal'
 
 interface DeviceCardProps {
     device: Device
 }
 
 const DeviceCard: React.FC<DeviceCardProps> = ({ device }) => {
+    const [showModal, setShowModal] = useState(false)
+
+    const openModal = () => {
+        setShowModal(prevState => !prevState)
+    }
+
     return(
         <div className="card col-3 border-danger">
-            <img className="card-img-top" src={ device.imageUrl } alt={ device.shortDescription }/>
+            <img className="card-img-top"
+                src={ device.imageUrl ? device.imageUrl : require('../../assets/img/default_device.png') } 
+                alt={ device.shortDescription }/>
             <div className="card-body">
                 <h5 className="card-title">{ device.name }</h5>
                 <div className="card-text">{ device.shortDescription }</div>
                 <div className="card-text">{ device.price } €</div>
             </div>
+            <div className="d-flex justify-content-end mb-3 me-3 gap-2">
+                <button type="button"
+                        title="Add to cart" 
+                        className="btn btn-danger">
+                            <i className="bi bi-cart-plus"/>
+                </button>
+                <button type="button" 
+                        className="btn btn-outline-danger"
+                        title="Preview" 
+                        onClick={ openModal }>
+                            <i className="bi bi-display"/>
+                </button>
+            </div>
+            { showModal && <DevicePreviewModal show={ showModal } onHide={ openModal } device={ device } /> }
         </div>
     )
 }
---
+++ src/devices/device-preview-modal/DevicePreviewModal.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react'
+import './DevicePreviewModal.css'
+import { Device } from '../../data-models/model/Device.model'
+
+interface DevicePreviewModalProps {
+    show: boolean
+    device: Device
+    onHide: () => void
+}
+
+const DevicePreviewModal: React.FC<DevicePreviewModalProps> = ({ show, device, onHide }) => {
+    const [deviceType, setDeviceType] = useState('')
+
+    const modifyDeviceType = () => {
+        const parts = device.type.split('_')
+        const modifiedType = parts.map(part => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()).join(' ')
+
+        setDeviceType(modifiedType)
+    }
+
+    useEffect(() => {
+        modifyDeviceType()
+    }, [])
+
+    return (
+        <div>
+            <div className={ `modal ${show ? 'show' : ''}` } 
+                tabIndex={ -1 } 
+                role="dialog" 
+                style={ { display: show ? 'block' : 'none' } }>
+                    <div className="modal-dialog" 
+                        role="document">
+                            <div className="modal-content">
+                                <div className="modal-header justify-content-center">
+                                    <h5 className="modal-title"> { device.name } </h5>
+                                    <button type="button" 
+                                            className="btn btn-outline-dark close-btn" 
+                                            onClick={ onHide }>
+                                                <i className="bi bi-x-lg"/>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <div className="card">
+                                        <img className="card-img-top"                 
+                                            src={ device.imageUrl ? device.imageUrl : require('../../assets/img/default_device.png') } 
+                                            alt={ device.shortDescription } />
+                                        <div className="card-body">
+                                            <div className="card-text">{ device.longDescription }</div>
+                                            <div className="card-text">Type: { deviceType }</div>
+                                            <div className="card-text">Price: { device.price } €</div>
+                                            <div className="card-text">Available: { device.available 
+                                                                            ? (<i className="bi bi-cart-check"/>) 
+                                                                            : (<i className="bi bi-cart-x"/>) }
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+
+                                </div>
+                            </div>
+
+                    </div>
+            </div>
+            <div className={ `modal-backdrop ${show ? 'show' : ''}` }></div>
+        </div>
+    )
+}
+
+export default DevicePreviewModal
